813ee1a3aada57e923cb80fc007a3b2eee85f428
vindyavahini <128346520+vindyav@users.noreply.github.com>
2024-08-30 15:26:35 +0530

    Update half_adder_tb.v

--- Half Adder/half_adder_tb.v
+++ Half Adder/half_adder_tb.v
@@ -2,17 +2,23 @@ module halfadder_tb;
 reg in_a, in_b;
 wire out_sum, out_carry;
 
-  halfadder add1(out_sum, out_carry, in_a, in_b);
-     
-initial     
-begin  
-  $monitor("a = %b  b = %b, Sum = %b, carry = %b\n",in_a, in_b,out_sum, out_carry);
-a = 0; b = 0;
-#1
-b = 1;
-#1
-a = 1;
-#1
-b = 0;
+
+halfadder add1(
+  .in_a(in_a),
+  .in_b(in_b),
+  .out_sum(out_sum),
+  .out_carry(out_carry)
+);
+
+initial begin  
+  $monitor("a = %b  b = %b, Sum = %b, carry = %b", in_a, in_b, out_sum, out_carry);
+  
+  in_a = 0; in_b = 0;
+  #1
+  in_b = 1;
+  #1
+  in_a = 1;
+  #1
+  in_b = 0;
 end
 endmodule
