4fcd37ddfcc8982221244c6a4ee2a4a7199ee933
vindyavahini <128346520+vindyav@users.noreply.github.com>
2024-08-30 15:05:36 +0530

    Update half_adder.v

--- Half Adder/half_adder.v
+++ Half Adder/half_adder.v
@@ -1,1 +1,20 @@
+module half_adder(
+	               in_a,         //input
+	               in_b,         //input 
+               	 out_sum,      //output
+	               out_carry     //output
+                  );
 
+//Declaring Ports
+
+input  in_a;                  //input declaration
+input  in_b;                  //input declaration
+output out_sum;               //output declaration
+output out_carry;             //output declaration
+  
+//Half adder's Sum and Carry Expressions
+
+assign out_sum   =  in_a ^ in_b;       //xor operation
+assign out_carry =  in_a & in_b;       //and operation
+  
+endmodule 
