20aceac85a99f5b9b5932e371e62a1502e1b768e
vindyavahini <128346520+vindyav@users.noreply.github.com>
2025-09-02 12:57:15 +0530

    Update Vectorgates.v

--- vectors/Vectorgates.v
+++ vectors/Vectorgates.v
@@ -1,14 +1,10 @@
 module top_module( 
-    input [2:0] a,
-    input [2:0] b,
-    output [2:0] out_or_bitwise,
-    output out_or_logical,
-    output [5:0] out_not
+    input [3:0] in,
+    output out_and,
+    output out_or,
+    output out_xor
 );
-    assign out_or_bitwise = b|a;
-    assign out_or_logical = b || a;
-    assign out_not = {~b, ~a};
-    /* assign out_not[5:3] = ~b;
-       assign out_not[2:0] = ~a;
-    */
+    assign out_and = &in;
+    assign out_or = |in;
+    assign out_xor = ^in;
 endmodule
