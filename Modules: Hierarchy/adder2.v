5328f08f085642ca11899c772f1ad95127f85197
vindyavahini <128346520+vindyav@users.noreply.github.com>
2025-09-04 17:23:26 +0530

    Update adder2.v

--- Modules: Hierarchy/adder2.v
+++ Modules: Hierarchy/adder2.v
@@ -3,12 +3,11 @@ module top_module (
     input [31:0] b,
     output [31:0] sum
 );
-    wire [15:0] s1, s2;
-    wire c1, c2;
+    wire c1;
     
-    add16 inst1 (.a(a[15:0]), .b(b[15:0]), .cin(c1), .sum(s1), .cout(c2));
-    add16 inst2 (.a(a[31:16]), .b(b[31:16]), .cin(c2), .sum(s2), .cout());
-    assign sum = {s2, s1};
+    add16 inst1 (.a(a[15:0]), .b(b[15:0]), .cin(1'b0), .sum(sum[15:0]), .cout(c1));
+    add16 inst2 (.a(a[31:16]), .b(b[31:16]), .cin(c1), .sum(sum[31:16]), .cout());
+    
 endmodule
 
 module add1 ( input a, input b, input cin,   output sum, output cout );
